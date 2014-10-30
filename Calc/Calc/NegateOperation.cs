17f047e87dcbfc52ade8dff939c28aef790ba825
Alex Novak <alexwnovak@gmail.com>
2014-10-30 17:07:16 -0400

    +NegateOperation empty class

---
+++ Calc/Calc/NegateOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace Calc
+{
+   public class NegateOperation : IUnaryExpression
+   {
+      public void Print()
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
