9b7e57c2777c0f947664d94421758c7e25c37390
Alex Novak <alexwnovak@gmail.com>
2014-10-30 17:13:43 -0400

    +Field and getter for expression

--- Calc/Calc/UnaryExpression.cs
+++ Calc/Calc/UnaryExpression.cs
@@ -2,6 +2,12 @@
 {
    public abstract class UnaryExpression : IExpression
    {
+      private readonly IExpression _expression;
+      protected IExpression Expression
+      {
+         get { return _expression; }
+      }
+
       public abstract void Print();
    }
 }
